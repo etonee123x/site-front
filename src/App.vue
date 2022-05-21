4181d57744a8502b053cb8fd520c41b9d4181860
etonee123x <e123wh@mail.ru>
2022-05-21 17:22:16 +0300

    creating working env

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
-<template></template>
+<template>
+  <div></div>
+</template>
 
 <script setup lang="ts"></script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import eslint from 'vite-plugin-eslint';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  plugins: [vue(), eslint({ cache: true })],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+      @import "./src/assets/styles/mixins.scss";
+      @import "./src/assets/styles/variables.scss";
+    `,
+      },
+    },
+  },
+});
