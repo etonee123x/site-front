defc438a33e149ae6ecd97184fcdd12dba98fd2e
Evgeniy Uvarov <etonee123x@gmail.com>
2024-02-23 16:58:56 +0400

    upd

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,19 +8,8 @@ export const formatDuration = (end: number): string => {
   return `${minutes}:${String(seconds).padStart(2, '0')}`;
 };
 
-export const to0To1Borders = (currentValue: number, [minValue = 0, maxValue]: [number | undefined, number]) => {
-  const result = (currentValue - minValue) / (maxValue - minValue);
-
-  if (result > 1) {
-    return 1;
-  }
-
-  if (result < 0) {
-    return 0;
-  }
-
-  return result;
-};
+export const to0To1Borders = (currentValue: number, [minValue = 0, maxValue]: [number | undefined, number]) =>
+  Math.max(0, Math.min(1, (currentValue - minValue) / (maxValue - minValue)));
 
 export const addId = <T>(el: T, id = Number(crypto.randomUUID().match(/[\d]/g)?.join(''))): WithId & T => ({
   ...el,
