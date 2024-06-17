c4c3a47679b36adc550b1bdf80d74607ca10c459
Evgeniy Uvarov <etonee123x@gmail.com>
2024-06-17 10:22:40 +0400

    upd

--- src/api/_client.ts
+++ src/api/_client.ts
@@ -1,7 +1,31 @@
 import { createFetch } from 'ofetch';
 
+import { TOKEN } from '@/constants';
+
+const STATUS_UNAUTHORIZED = 401;
+
 export const client = createFetch({
   defaults: {
     baseURL: import.meta.env.VITE_API_URL,
+    onRequest({ options }) {
+      const maybeToken = localStorage.getItem(TOKEN);
+
+      options.headers = {
+        ...options.headers,
+        ...(maybeToken
+          ? {
+              Authorization: maybeToken,
+            }
+          : {}),
+      };
+    },
+    onResponseError({ response }) {
+      if (response.status !== STATUS_UNAUTHORIZED) {
+        return;
+      }
+
+      localStorage.removeItem(TOKEN);
+      window.location.href = '/';
+    },
   },
 });
