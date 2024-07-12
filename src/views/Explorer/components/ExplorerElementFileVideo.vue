714f583ad42a52c4b2956b50956f93b07feeac4a
martsdag <clarte.lisse.31@gmail.com>
2024-07-12 18:12:51 +0300

    fix

--- src/views/Explorer/components/ExplorerElementFileVideo.vue
+++ src/views/Explorer/components/ExplorerElementFileVideo.vue
@@ -1,5 +1,5 @@
 <template>
-  <ElementFileWrapper :element="element" @keydown.enter="onClick" @click="onClick">
+  <ElementFileWrapper :element="element">
     <div class="flex justify-center max-w-full">
       <video class="max-w-full" :src="element.src" controls />
     </div>
@@ -11,13 +11,7 @@ import { type ItemVideo } from '@shared/src/types';
 
 import ElementFileWrapper from './_ElementFileWrapper.vue';
 
-import { useGalleryStore } from '@/stores/gallery';
-
-const props = defineProps<{
+defineProps<{
   element: ItemVideo;
 }>();
-
-const { loadVideo } = useGalleryStore();
-
-const onClick = () => loadVideo(props.element);
 </script>
